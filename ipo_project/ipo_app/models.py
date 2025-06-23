f899837796b6fa27b680334b127f428cb0799f04
prreff123 <you@example.com>
2025-06-23 14:15:34 +0530

    ipo

--- ipo_project/ipo_app/models.py
+++ ipo_project/ipo_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from datetime import date
 class IPO(models.Model):
     STATUS_CHOICES = [
         ('Upcoming', 'Upcoming'),
@@ -7,6 +7,18 @@ class IPO(models.Model):
         ('Listed', 'Listed'),
         ('Ongoing', 'Ongoing'),
     ]
+
+    def auto_update_status(self):
+        today = date.today()
+        if today < self.opening_date:
+            self.status = 'Upcoming'
+        elif self.opening_date <= today <= self.closing_date:
+            self.status = 'Ongoing'
+        elif today > self.closing_date and today < self.listing_date:
+            self.status = 'Closed'
+        elif today >= self.listing_date:
+            self.status = 'Listed'
+        self.save()
 
     company_name = models.CharField(max_length=255)
     logo = models.ImageField(upload_to='media/', null=True, blank=True)
@@ -22,8 +34,8 @@ class IPO(models.Model):
     listing_gain = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
     current_market_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     current_return = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    rhp_pdf = models.FileField(upload_to='pdfs/', null=True, blank=True)
-    drhp_pdf = models.FileField(upload_to='pdfs/', null=True, blank=True)
+    rhp_pdf = models.FileField(upload_to='pdfs/rhp/', null=True, blank=True)
+    drhp_pdf = models.FileField(upload_to='pdfs/drhp/', null=True, blank=True)
 
     def __str__(self):
         return self.company_name
